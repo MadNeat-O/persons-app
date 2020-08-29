ec71072baa2ff85df17781beb8b44cf1da9b54fb
Brad Korman <brad.korman@remarque-creative.com>
2020-08-29 01:21:45 -0400

    added cockpit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
-import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+import Persons from '../components/Persons/Persons';
 
 class App extends Component {
   state = {
@@ -52,7 +51,12 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
         <div>
-          {this.state.persons.map( ( person, index ) => {
+          <Persons 
+            persons={this.state.persons} 
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler} 
+          />
+          {/* {this.state.persons.map( ( person, index ) => {
             return <ErrorBoundary key={person.id}>
               <Person
                 click={() => this.deletePersonHandler( index )}
@@ -60,7 +64,7 @@ class App extends Component {
                 age={person.age}
                 changed={( event ) => this.nameChangedHandler( event, person.id )} />
             </ ErrorBoundary>
-          } )}
+          } )} */}
         </div>
       );
 
